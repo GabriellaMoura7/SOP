b558278bae487d1a6e16a7bbed873fc39fb0af16
Gabriella Del Vigna de Moura <159180575+GabriellaMoura7@users.noreply.github.com>
2024-04-11 10:50:33 -0300

    Add files via upload

---
+++ A03-Criação de .bat e Agendamento de tarefa.bat
@@ -0,0 +1,10 @@
+Criando uma Tarefa
+
+@echo off
+@echo ********************************
+@echo ***                          ***
+@echo ***    Hora Do Almoco :D     ***
+@echo ***                          ***
+@echo ********************************
+@echo.
+pause
